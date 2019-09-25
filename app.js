5dc4121248e3ff62eb12219602576727440aa6c3
aeam88 <a.alvarezmontes@gmail.com>
2019-09-25 14:21:37 -0300

    Middleware Auth, rutas y model usuario

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicio variables
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // DB Connect
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -12,16 +20,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
+
 app.listen(3000, () => {
     console.log('Express server online port 3000');
 });
 
-// Rutas
-app.get('/', (req, res, next) => {
-    
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-
-});
